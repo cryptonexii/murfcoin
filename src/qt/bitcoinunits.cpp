e897b15d20d29abaaa4e8cea9da4fd8168738e00
Crypto Nexii <cryptonexii@gmail.com>
2018-01-22 00:34:16 -0800

    Proper MURF Units

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -41,8 +41,8 @@ QString BitcoinUnits::name(int unit)
     switch(unit)
     {
     case BTC: return QString("FLUFF");
-    case mBTC: return QString("lites");
-    case uBTC: return QString("photons");
+    case mBTC: return QString("fluffies");
+    case uBTC: return QString("flufferinos");
     default: return QString("???");
     }
 }
@@ -51,9 +51,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Murfcoins");
-    case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("FLUFF");
+    case mBTC: return QString("Fluffies (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Flufferinos (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
