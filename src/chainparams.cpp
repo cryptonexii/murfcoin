0b9a55ced3a45aa8e751a6da7590f7d213da4deb
Crypto Nexii <cryptonexii@gmail.com>
2018-01-23 14:59:29 -0800

    Actiovate Segwit

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -94,13 +94,13 @@ public:
 
         // Deployment of BIP68, BIP112, and BIP113.
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1516740679; // January 28, 2017
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1548276678; // January 31st, 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1516748231; // January 23, 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1517353031; // January 30th, 2018
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1516740679; // January 28, 2017
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1548276678; // January 31st, 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1516748231; // January 23, 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1517353031; // January 30th, 2018
 
 
         // The best chain should have at least this much work.
